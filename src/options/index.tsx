1dbd00a3481a8a1a26b33276364a976b86426253
MarioJim <mario.emilio.j@gmail.com>
2021-03-12 14:50:01 -0600

    Remove options page

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Options from './Options';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Options />
-  </React.StrictMode>,
-  document.getElementById('options')
-);
+export {};
